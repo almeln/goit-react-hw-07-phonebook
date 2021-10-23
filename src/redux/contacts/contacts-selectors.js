6b826f97197b04ed989a0fef55589eb1407b31a5
Alina Melnyk <alinkaaa345@gmail.com>
2021-10-23 23:26:04 +0300

    adds memoizaton

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,14 +1,24 @@
-// import { fetchContacts } from "./contacts-operations";
+import { createSelector } from '@reduxjs/toolkit';
 
 export const getItems = state => state.contacts.items;
 export const getFilter = state => state.contacts.filter;
 
-export const getVisibleContacts = state => {
-  const items = getItems(state);
-  // const items = fetchContacts();
-  const filter = getFilter(state);
-  const normalizedFilter = filter.toLowerCase();
-  return items.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter),
-  );
-};
+// Мемоизация
+export const getVisibleContacts = createSelector(
+  [getItems, getFilter],
+  (items, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return items.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  },
+);
+
+// export const getVisibleContacts = state => {
+//   const items = getItems(state);
+//   const filter = getFilter(state);
+//   const normalizedFilter = filter.toLowerCase();
+//   return items.filter(contact =>
+//     contact.name.toLowerCase().includes(normalizedFilter),
+//   );
+// };
